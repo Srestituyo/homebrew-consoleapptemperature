1501f15ea00b589e48a8d2e2b944dbd33053b7ac
Srestituyo <sebastianrestituyo@gmail.com>
2022-09-14 17:52:50 -0400

    updaing formula

--- consoleapptemperature.rb
+++ consoleapptemperature.rb
@@ -1,6 +1,4 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
 class Consoleapptemperature < Formula
   desc ""
   homepage ""
@@ -11,19 +9,7 @@ class Consoleapptemperature < Formula
   # depends_on "cmake" => :build
 
   def install
-    bin.install "katatemperatureconsoleapp"
+    bin.install "consoleapptemperature"
   end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test ConsoleAppTemperature`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
+ 
 end
