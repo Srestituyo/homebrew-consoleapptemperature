50a35f9c65bec2a7d7066d03c956efc2391a2caa
Srestituyo <49099635+Srestituyo@users.noreply.github.com>
2022-09-28 17:21:20 -0400

    Update Temperaturekataconsolapp.rb

--- Temperaturekataconsolapp.rb
+++ Temperaturekataconsolapp.rb
@@ -2,7 +2,7 @@
 class Consoleapptemperature < Formula
   desc ""
   homepage ""
-  url "https://github.com/Srestituyo/ConsoleAppTemperature/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/Srestituyo/ConsoleAppTemperature/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "305ab30879eff8374c18b9af03d23c8fa9b35c071774cf03d6b83ebe8b9308f0"
   license ""
 
